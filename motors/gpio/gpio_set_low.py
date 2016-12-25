33081bb489ade7988fd120a57cc887ccb6994fe7
Alexandre Iooss <alexandre.iooss@gmail.com>
2016-12-25 17:52:40 +0100

    Add GPIO scripts

---
+++ motors/gpio/gpio_set_low.py
@@ -0,0 +1,16 @@
+## http://deusyss.developpez.com/tutoriels/RaspberryPi/PythonEtLeGpio/
+
+import RPi.GPIO as GPIO
+
+n = 2  # GPIO number
+
+GPIO.setmode(GPIO.BCM)  # Follow broadcom convention
+
+print(GPIO.getmode())  # Print the conf
+
+print("Was configured as ", GPIO.gpio_function(n))
+GPIO.setup(n, GPIO.OUT)  # Set as OUT
+print("Now configured as ", GPIO.gpio_function(n))
+
+GPIO.output(n, GPIO.OUT)  # Set as OUT
+
